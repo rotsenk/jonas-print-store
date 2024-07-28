50b344851b2dc56194f0b74901848744f72bcf5e
rotsenk <stanley.95@hotmail.com>
2024-07-28 09:17:05 -0600

    images to grid

--- src/components/photoGrid.jsx
+++ src/components/photoGrid.jsx
@@ -5,52 +5,52 @@ function PhotoGrid() {
     <>
       <div className="w3-row-padding w3-padding-16 w3-center" id="food">
         <div className="w3-quarter">
-          <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{ width: "100%" }} />
-          <h3>The Perfect Sandwich, A Real NYC Classic</h3>
+          <img src="src/assets/logos/charm-style.png" alt="charm-style" style={{ width: "100%" }} />
+          <h3>Charm Style</h3>
           <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/steak.jpg" alt="Steak" style={{ width: "100%" }} />
-          <h3>Let Me Tell You About This Steak</h3>
+          <img src="src/assets/logos/colorcraft-logo.png" alt="colorcraft-logo" style={{ width: "100%" }} />
+          <h3>Colorcraft</h3>
           <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/cherries.jpg" alt="Cherries" style={{ width: "100%" }} />
-          <h3>Cherries, interrupted</h3>
+          <img src="src/assets/logos/iap-logo.png" alt="iap-logo" style={{ width: "100%" }} />
+          <h3>Innovative A. Projects</h3>
           <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
           <p>What else?</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/wine.jpg" alt="Pasta and Wine" style={{ width: "100%" }} />
-          <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
+          <img src="src/assets/logos/sharp-stylish.png" alt="sharp-stylish-logo" style={{ width: "100%" }} />
+          <h3>Sharp Stylish</h3>
           <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
       </div>
 
       <div className="w3-row-padding w3-padding-16 w3-center">
         <div className="w3-quarter">
-          <img src="/w3images/popsicle.jpg" alt="Popsicle" style={{ width: "100%" }} />
-          <h3>All I Need Is a Popsicle</h3>
+          <img src="src/assets/logos/glamour-salon.png" alt="glamour-salon" style={{ width: "100%" }} />
+          <h3>Glamour Salon</h3>
           <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/salmon.jpg" alt="Salmon" style={{ width: "100%" }} />
-          <h3>Salmon For Your Skin</h3>
+          <img src="src/assets/logos/bdc.png" alt="bdc-logo" style={{ width: "100%" }} />
+          <h3>Building Dreams</h3>
           <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{ width: "100%" }} />
-          <h3>The Perfect Sandwich, A Real Classic</h3>
+          <img src="src/assets/logos/ssc.png" alt="ssc-logo" style={{ width: "100%" }} />
+          <h3>Solid Structures</h3>
           <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
         <div className="w3-quarter">
-          <img src="/w3images/croissant.jpg" alt="Croissant" style={{ width: "100%" }} />
-          <h3>Le French</h3>
+          <img src="src/assets/logos/buildwise-logo.png" alt="buildwise-logo" style={{ width: "100%" }} />
+          <h3>Buildwise C.</h3>
           <p>Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
         </div>
       </div>
 
-      <div className="w3-center w3-padding-32">
+      {/* <div className="w3-center w3-padding-32">
         <div className="w3-bar">
           <a href="#" className="w3-bar-item w3-button w3-hover-black">
             «
@@ -71,7 +71,7 @@ function PhotoGrid() {
             »
           </a>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
